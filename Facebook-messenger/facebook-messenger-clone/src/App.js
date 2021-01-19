17cb62f6cd2b5209710e595375868cc06d3c3c49
AnjaliG1999 <anjali24.7goyal@gmail.com>
2021-01-20 01:29:35 +0530

    Clean up the look, online hosting

--- Facebook-messenger/facebook-messenger-clone/src/App.js
+++ Facebook-messenger/facebook-messenger-clone/src/App.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
-import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+import { FormControl, Input, IconButton } from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
 import Message from './Message';
 import db from './firebase';
 import firebase from 'firebase';
+import FlipMove from 'react-flip-move';
 import './App.css';
 
 function App() {
@@ -16,9 +18,9 @@ function App() {
   // listener within a listener
   // useEffect runs when page loads, onSnapshot runs every time db changes
   useEffect(() => {
-    db.collection('messages').orderBy('timestamp', 'asc')
+    db.collection('messages').orderBy('timestamp', 'desc')
     .onSnapshot(snapshot => {
-      setMessages(snapshot.docs.map(doc => doc.data()))
+      setMessages(snapshot.docs.map(doc => ({id: doc.id, message: doc.data()})))
     });
   }, [] )
 
@@ -41,22 +43,27 @@ function App() {
 
   return (
     <div className="App">
+      <img src='https://blueprint-api-production.s3.amazonaws.com/uploads/story/thumbnail/120380/1d104deb-1dc0-4633-b09b-1c43ba85ac3c.jpg'
+        alt="logo" width="200px" padding-top="10px"/>
       <h1>Hello Clever Programmers 🚀!!</h1>
-      <h2>Welcome {username}</h2>
+      <h2>Welcome {username || 'Anonymous'}</h2>
 
-      <form>
-        <FormControl>
-          <InputLabel>Enter a message...</InputLabel>
-          <Input value={input} onChange={event => setInput(event.target.value)} />
-        </FormControl>  
-        <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>Send Message</Button>     
+      <form className="app_form">
+        <FormControl className="app_formControl">
+          <Input className="app_input" placeholder="Enter a message..." value={input} onChange={event => setInput(event.target.value)} />
+          <IconButton className="app_iconButton" disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>
+            <SendIcon />
+          </IconButton> 
+        </FormControl>     
       </form>
 
-      {
-        messages.map(message => {
-          return <Message username={username} message={message}/>
-        })
-      }
+      <FlipMove>
+        {
+          messages.map(({id, message}) => {
+            return <Message key={id} username={username} message={message}/>
+          })
+        }
+      </FlipMove>
     </div>
   );
 }
