0d080126a51f96d9e7f0de2594d48c0db031818c
Anjali Goyal <anjali24.7goyal@gmail.com>
2021-04-22 23:18:42 +0530

    Static amazon clone header bar

---
+++ Amazon-Clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAo7k0-fF0uJmlxIlUmJPtn_BFtUOMoBqw",
+  authDomain: "clone-771b7.firebaseapp.com",
+  projectId: "clone-771b7",
+  storageBucket: "clone-771b7.appspot.com",
+  messagingSenderId: "625986087746",
+  appId: "1:625986087746:web:509f8e628ad7a910e476db",
+  measurementId: "G-RBGTZ4NJMN",
+};
