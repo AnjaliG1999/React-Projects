6ab4cd8cbe88950cc51041d6b4fc2d0de22d7194
Anjali Goyal <anjali24.7goyal@gmail.com>
2021-05-31 03:36:15 +0530

    Cake modal and styles

--- Anjali-cakeshop/src/components/home/Cakedetails.js
+++ Anjali-cakeshop/src/components/home/Cakedetails.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 
 import { Link, useParams } from "react-router-dom";
 import StarRatings from "react-star-ratings";
@@ -8,8 +8,17 @@ import addToCart from "../../Addtocart";
 import axios from "../../axios";
 
 import "../../css/Cakedetails.css";
-import { Breadcrumbs, TextField, Typography } from "@material-ui/core";
-import { KeyboardDateTimePicker } from "@material-ui/pickers";
+import {
+  Breadcrumbs,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Slide,
+  TextField,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core";
 import {
   NavigateNext,
   VerifiedUser,
@@ -17,10 +26,28 @@ import {
   Security,
   LocalOffer,
   KeyboardArrowDown,
+  HighlightOff,
 } from "@material-ui/icons";
+import CakeTabs from "./CakeTabs";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
 
 function Cakedetails(props) {
-  const [selectedDate, handleDateChange] = useState(new Date());
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // const [selectedDate, handleDateChange] = useState(new Date());
   const cakeToCart = () => {
     var cake = {
       email: localStorage.email,
@@ -29,7 +56,10 @@ function Cakedetails(props) {
       price: data.cake.price,
       weight: data.cake.weight,
       image: data.cake.image,
+      // message: document.querySelector("#cakeMsg").value,
+      // date: document.querySelector("#cakeDate").value,
     };
+    // console.log(cake);
     addToCart(props, cake);
   };
 
@@ -69,6 +99,7 @@ function Cakedetails(props) {
         <div className="cakedata__imgContainer">
           <img
             className="cakedata__img"
+            onClick={handleClickOpen}
             onError={(e) => {
               e.target.onError = null;
               e.target.src =
@@ -77,9 +108,43 @@ function Cakedetails(props) {
             src={cake.image}
             alt="Error loading cake"
           />
+          <Dialog
+            fullScreen={fullScreen}
+            open={open}
+            TransitionComponent={Transition}
+            keepMounted
+            onClose={handleClose}
+            aria-labelledby="responsive-dialog-title"
+          >
+            <DialogTitle id="responsive-dialog-title">
+              {cake.name}{" "}
+              <HighlightOff
+                onClick={handleClose}
+                style={{
+                  position: "absolute",
+                  right: "20px",
+                  cursor: "pointer",
+                }}
+              />
+            </DialogTitle>
+            <DialogContent>
+              <img
+                className="cakedata__img"
+                width="500px"
+                onClick={handleClickOpen}
+                onError={(e) => {
+                  e.target.onError = null;
+                  e.target.src =
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png";
+                }}
+                src={cake.image}
+                alt="Error loading cake"
+              />
+            </DialogContent>
+          </Dialog>
         </div>
         <div className="cakedata__zoom">
-          <p style={{ color: "gray" }}>{cake.cakeid}</p>
+          <p style={{ color: "#2b1d0e" }}>{cake.cakeid}</p>
           <p>Click on the image to zoom in</p>
         </div>
 
@@ -87,13 +152,14 @@ function Cakedetails(props) {
           <input
             type="text"
             className="cakedata__message"
+            id="cakeMsg"
             placeholder="Message On Cake"
           />
           <TextField
-            id="datetime-local"
             label="Delivery Date"
+            id="cakeDate"
             type="datetime-local"
-            defaultValue={new Date().toISOString().substr(0, 16)}
+            // defaultValue={new Date().toISOString().substr(0, 16)}
             className="cakedata__date"
             InputLabelProps={{
               shrink: true,
@@ -103,6 +169,9 @@ function Cakedetails(props) {
           <button type="button" onClick={cakeToCart} className="btn">
             Add to Cart
           </button>
+          {/* <button type="button" className="btn">
+            Order Cake
+          </button> */}
         </div>
       </div>
       <div className="cakedata__right">
@@ -138,156 +207,7 @@ function Cakedetails(props) {
           <small style={{ paddingLeft: "10%" }}>Inclusive of all prices</small>
         </div>
 
-        <ul
-          className="nav nav-tabs nav-justified"
-          id="myTab"
-          role="tablist"
-          style={{ marginTop: "5%" }}
-        >
-          <li className="nav-item" role="presentation">
-            <button
-              className="nav-link active"
-              id="description-tab"
-              data-bs-toggle="tab"
-              data-bs-target="#description"
-              type="button"
-              role="tab"
-              aria-controls="description"
-              aria-selected="true"
-            >
-              Description
-            </button>
-          </li>
-          <li className="nav-item" role="presentation">
-            <button
-              className="nav-link"
-              id="delivery-tab"
-              data-bs-toggle="tab"
-              data-bs-target="#delivery"
-              type="button"
-              role="tab"
-              aria-controls="delivery"
-              aria-selected="false"
-            >
-              Delivery Information
-            </button>
-          </li>
-          <li className="nav-item" role="presentation">
-            <button
-              className="nav-link"
-              id="care-tab"
-              data-bs-toggle="tab"
-              data-bs-target="#care"
-              type="button"
-              role="tab"
-              aria-controls="care"
-              aria-selected="false"
-            >
-              Care Instructions
-            </button>
-          </li>
-        </ul>
-        <div className="tab-content" id="myTabContent">
-          <div
-            className="tab-pane fade show active"
-            id="description"
-            role="tabpanel"
-            aria-labelledby="description-tab"
-          >
-            <div className="cake__description">
-              <div className="cakedata__details">
-                <h5>Cake Details:</h5>
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item">
-                    <strong>Flavour:</strong> {cake.flavour}
-                  </li>
-                  <li className="list-group-item">
-                    <strong>Weight:</strong> {cake.weight} pounds
-                  </li>
-                  <li className="list-group-item">
-                    <strong>Shape:</strong> Circle
-                  </li>
-                  <li className="list-group-item">
-                    <strong>Serves:</strong> 4-6 People
-                  </li>
-                  <li className="list-group-item">
-                    <strong>Type:</strong> {cake.type} Cake
-                  </li>
-                </ul>
-              </div>
-
-              <div className="cakedata__about" style={{ flex: 2 }}>
-                <h5>About the Cake:</h5>
-                {/* truncate description as well? */}
-                <p style={{ overflowWrap: "anywhere" }}>{cake.description}</p>
-              </div>
-            </div>
-          </div>
-          <div
-            className="tab-pane fade"
-            id="delivery"
-            role="tabpanel"
-            aria-labelledby="delivery-tab"
-          >
-            <ul className="list-group list-group-flush cake__description">
-              <li className="list-group-item">
-                Every cake we offer is handcrafted and since each chef has
-                his/her own way of baking and designing a cake, there might be
-                slight variation in the product in terms of design and shape.
-              </li>
-              <li className="list-group-item">
-                The chosen delivery time is an estimate and depends on the
-                availability of the product and the destination to which you
-                want the product to be delivered.{" "}
-              </li>
-              <li className="list-group-item">
-                Since cakes are perishable in nature, we attempt delivery of
-                your order only once. The delivery cannot be redirected to any
-                other address.
-              </li>
-              <li className="list-group-item">
-                This product is hand delivered and will not be delivered along
-                with courier products.
-              </li>
-              <li className="list-group-item">
-                Occasionally, substitutions of flavours/designs is necessary due
-                to temporary and/or regional unavailability issues.
-              </li>
-            </ul>
-          </div>
-          <div
-            className="tab-pane fade"
-            id="care"
-            role="tabpanel"
-            aria-labelledby="care-tab"
-          >
-            <ul className="list-group list-group-flush cake__description">
-              <li className="list-group-item">
-                Store cream cakes in a refrigerator. Fondant cakes should be
-                stored in an air conditioned environment.
-              </li>
-              <li className="list-group-item">
-                Slice and serve the cake at room temperature and make sure it is
-                not exposed to heat.
-              </li>
-              <li className="list-group-item">
-                Use a serrated knife to cut a fondant cake.
-              </li>
-              <li className="list-group-item">
-                Sculptural elements and figurines may contain wire supports or
-                toothpicks or wooden skewers for support.
-              </li>
-              <li className="list-group-item">
-                Sculptural elements and figurines may contain wire supports or
-                toothpicks or wooden skewers for support.
-              </li>
-              <li className="list-group-item">
-                The cake should be consumed within 24 hours.
-              </li>
-              <li className="list-group-item">Enjoy your cake!</li>
-            </ul>
-          </div>
-        </div>
+        <CakeTabs cake={cake} />
 
         <div className="cakedata__safety">
           <p className="cakedata__safe">
